f9c5bb565533ed300bea79428d454a05a51dbae5
Jessica Grinberg <jessgrinberg@gmail.com>
2015-08-22 13:28:16 -0700

    change port to work on production

--- server.js
+++ server.js
@@ -165,6 +165,8 @@ app.use(bodyParser.json());
 var apiRouter = require('./app/config/routes')
 app.use(apiRouter);
 
-app.listen(7000);
+var port = process.env.PORT || 7000
 
-console.log('Server is running on port 7000');
+app.listen(port);
+
+console.log('Server is running on port', port);
